658876deabfb1fe8a66a00a8979edbd0764ef260
Evgenii <lipatowqevgeniy@gmail.com>
2019-09-27 03:57:44 -0400

    First stage

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task, Type, Status
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'brief', 'description', 'status','type','created_at']
+    list_filter = ['status', 'type']
+    list_display_links = ['pk', 'brief']
+    search_fields = ['title', 'text']
+    readonly_fields = ['created_at']
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    brief = models.CharField(max_length=100, null=False, blank=False, verbose_name='Brief description')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Description')
+    status = models.ForeignKey('Status',on_delete=models.PROTECT)
+    type = models.ForeignKey('Type',on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation Time')
+
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Status')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "statuses"
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Type')
+
+    def __str__(self):
+        return self.name
+
