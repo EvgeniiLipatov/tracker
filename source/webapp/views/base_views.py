8f455969a3fbd7f3d2ee3e0f6ef54a88acfc2fed
Evgenii <lipatowevgeniy@gmail.com>
2019-10-06 18:26:50 +0600

    Added var for  pk in base class view

--- source/webapp/views/base_views.py
+++ source/webapp/views/base_views.py
@@ -6,13 +6,15 @@ class BaseView(TemplateView):
     context_key = 'object'
     template_name = None
     model = None
+    key = 'pk'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context[self.context_key] = self.get_object(**kwargs)
+        context[self.context_key] = self.get_object()
 
         return context
 
-    def get_object(self, **kwargs):
-        return get_object_or_404(self.model, pk=kwargs['pk'])
+    def get_object(self):
+        pk = self.kwargs.get(self.key)
+        return get_object_or_404(self.model, pk=pk)
