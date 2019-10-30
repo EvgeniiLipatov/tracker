fff8d56cf0afa2960fde9a88fe7a7218e5ae3699
Evgenii <lipatowqevgeniy@gmail.com>
2019-10-30 10:14:54 -0400

    Added Personal Area to Tracker app.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import login_view, logout_view, register_view, user_activate_view
+from .views import login_view, logout_view, register_view, user_activate_view, UserChangeView, UserChangePasswordView, \
+    UserDetailView
 
 app_name = 'accounts'
 
@@ -7,5 +8,8 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
-    path('activate/<token>/', user_activate_view, name='user_activate')
+    path('activate/<token>/', user_activate_view, name='user_activate'),
+    path('profile/<pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('profile/<pk>/edit/', UserChangeView.as_view(), name='user_update'),
+    path('profile/<pk>/change-password/', UserChangePasswordView.as_view(), name='user_change_password')
 ]
