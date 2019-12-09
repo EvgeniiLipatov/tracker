731e2c8c4a27408d865de4677a9b1f6aa38e352d
Evgenii <lipatowqevgeniy@gmail.com>
2019-12-09 07:33:11 -0500

    Added the second task

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -40,7 +40,7 @@ function getTasks() {
 
 function getProjectTasks() {
     $.ajax({
-         url: 'http://127.0.0.1:8000/api/projects/2',
+         url: 'http://127.0.0.1:8000/api/projects/2/',
          method: 'get',
          headers: {'Authorization': 'Token ' + localStorage.getItem('apiToken')},
          data: JSON.stringify({username: 'admin', password: 'admin'}),
@@ -52,10 +52,52 @@ function getProjectTasks() {
 
 }
 
+function CreateTask() {
+    data = {
+        username:"fedya",
+        password: "M1234q",
+        "brief": "test1000",
+        "description": "just_for_fun_11",
+        "status": 2,
+        "type": 1,
+        "project": 1,
+       // "created_at": "2019-12-09T11:50:50.694108Z",
+       // "updated_at": "2019-12-09T11:50:50.694159Z",
+        "created_by": 1
+
+    };
+    $.ajax({
+         url: 'http://127.0.0.1:8000/api/tasks/',
+         method: 'post',
+         headers: {'Authorization': 'Token ' + localStorage.getItem('apiToken')},
+         //data: JSON.stringify({username: 'fedya', password: 'M1234q'}),
+         data: JSON.stringify(data),
+         dataType: 'json',
+         contentType: 'application/json',
+         success: function(response, status){console.log(response);},
+         error: function(response, status){console.log(response);}
+    });
+
+}
+
+function DeleteTask() {
+    $.ajax({
+         url: 'http://127.0.0.1:8000/api/tasks/11',
+         method: 'delete',
+         headers: {'Authorization': 'Token ' + localStorage.getItem('apiToken')},
+         dataType: 'json',
+         contentType: 'application/json',
+         success: function(response, status){console.log("Deleted");},
+         error: function(response, status){console.log("Not deleted");}
+    });
+
+}
 
 $(document).ready(function() {
    getToken();
    getProjects();
    getTasks();
    getProjectTasks();
+   CreateTask();
+   DeleteTask();
 });
